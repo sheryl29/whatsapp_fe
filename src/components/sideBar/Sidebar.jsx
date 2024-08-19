3bc9f71e95337319f291228920be2e6c27877e98
sheryl <sheirawadigau@gmail.com>
2024-08-19 15:29:28 +0700

    home layout and sidebar hearder

---
+++ src/components/sideBar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { SidebarHeader } from "./header";
+
+export default function Sidebar() {
+  return (
+    <div className="flex0030 max-w-[30%] h-full select-none">
+        {/* sidebar header */}
+
+        <SidebarHeader />    
+    </div>
+  )
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,17 @@
+import { Sidebar } from "../components/sideBar"
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center overflow-hidden">
+      {/* container */}
+      
+      <div className="container h-screen flex py-[19px]">
+
+        <Sidebar />
+
+      </div>
+
+
+    </div>
   )
 }
