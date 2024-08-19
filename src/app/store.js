d948647f70ea3af1b73a38988d5d219951c4876f
sheryl <sheirawadigau@gmail.com>
2024-08-19 16:59:45 +0700

    get conversations

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { persistReducer, persistStore } from "redux-persist";
 import userSlice from "../features/userSlice";
 import storage from "redux-persist/lib/storage";
 import createFilter from "redux-persist-transform-filter"
+import chatSlice from "../features/chatSlice";
 
 //save user only filter
 const saveUserOnlyFilter = createFilter("user", ["user"]);
@@ -17,6 +18,7 @@ const persistConfig ={
 
 const rootReducer = combineReducers({
     user: userSlice,
+    chat: chatSlice,
 });
 const persistedReducer=persistReducer(persistConfig, rootReducer);
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,20 @@
+
+import { useDispatch,useSelector } from "react-redux"
 import { Sidebar } from "../components/sideBar"
+import { useEffect } from "react";
+import { getConversations } from "../features/chatSlice";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const {user} = useSelector((state) => state.user);
+
+  //get conversations
+  useEffect(() => {
+    if(user?.token){
+      dispatch(getConversations(user.token));
+    }
+  }, [user]);
+
   return (
     <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center overflow-hidden">
       {/* container */}
