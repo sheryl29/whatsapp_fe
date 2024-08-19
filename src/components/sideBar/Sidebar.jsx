bf86a3912f7796e8a4ed0c7227b017ad1a22dd3d
sheryl <sheirawadigau@gmail.com>
2024-08-19 15:52:02 +0700

    sidebar notifications

--- src/components/sideBar/Sidebar.jsx
+++ src/components/sideBar/Sidebar.jsx
@@ -1,11 +1,15 @@
 import { SidebarHeader } from "./header";
+import { Notification } from "./notifications";
 
 export default function Sidebar() {
   return (
     <div className="flex0030 max-w-[30%] h-full select-none">
         {/* sidebar header */}
-
-        <SidebarHeader />    
+        <SidebarHeader />
+        
+        {/* notifications */}
+        <Notification />
+        
     </div>
   )
 }
---
+++ src/components/sideBar/notifications/index.js
@@ -0,0 +1,3 @@
+import Notification from "./Notifications";
+
+export{Notification};
