6868bc229cf7eeacfb604190ce360c1cbb3204bc
sheryl <sheirawadigau@gmail.com>
2024-08-19 16:17:35 +0700

    sidebar search

--- src/components/sideBar/Sidebar.jsx
+++ src/components/sideBar/Sidebar.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import { SidebarHeader } from "./header";
 import { Notification } from "./notifications";
+import { Search } from "./search";
 
 export default function Sidebar() {
+
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="flex0030 max-w-[30%] h-full select-none">
         {/* sidebar header */}
@@ -9,7 +14,12 @@ export default function Sidebar() {
         
         {/* notifications */}
         <Notification />
-        
+
+        {/* search */}
+        <Search 
+          searchLength={searchResults.length}
+          setSearchResults={setSearchResults}
+        />
     </div>
   )
 }
