abe5cd45762af7b3f5100b1b076fff6426323b2b
sheryl <sheirawadigau@gmail.com>
2024-08-28 23:30:30 +0800

    fixing styling problem

--- src/pages/home.js
+++ src/pages/home.js
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <div className="h-screen dark:bg-dark_bg_1 flex items-center justify-center overflow-hidden">
       {/*container*/}
-      <div className="container h-screen flex">
+      <div className="container h-screen flex py-[19px]">
         {/*Sidebar*/}
         <Sidebar />
         { activeConversation._id ? <ChatContainer /> : <WhatsappHome /> }
