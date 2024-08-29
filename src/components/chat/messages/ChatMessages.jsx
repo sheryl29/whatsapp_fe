ca63610b092dad75de818061fa499345273ab4bc
sheryl <sheirawadigau@gmail.com>
2024-08-29 22:16:30 +0800

    sending files

--- src/components/chat/messages/ChatMessages.jsx
+++ src/components/chat/messages/ChatMessages.jsx
@@ -1,39 +1,55 @@
-import { useSelector } from "react-redux"
-import Message from "./Message";
 import { useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import Message from "./Message";
 import Typing from "./Typing";
+import FileMessage from "./files/FileMessage.jsx";
 
-export default function ChatMessages({typing}) {
+export default function ChatMessages({ typing }) {
   const { messages, activeConversation } = useSelector((state) => state.chat);
   const { user } = useSelector((state) => state.user);
   const endRef = useRef();
-  useEffect(()=>{
+  useEffect(() => {
     scrollToBottom();
-  }, [messages]);
-  const scrollToBottom=() => {
-    endRef.current.scrollIntoView({behavior : "smooth"});
-  }
+  }, [messages, typing]);
+  const scrollToBottom = () => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-      <div
-        className="mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')]
-        bg-cover bg-no-repeat
-        "
-      >
-        {/* container */}
-        <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[5%]">
-            {/* messages */}
-            {
-              messages && messages.map((message)=>(
-                <Message 
-                  message={message} 
-                  key={message._id} 
-                  me={user._id === message.sender._id}/>
-              ))}
-              {typing === activeConversation._id ? <Typing /> : null}
-            <div className="mt-2" ref={endRef}>
-
-            </div>
-        </div>
+    <div
+      className="mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')]
+    bg-cover bg-no-repeat
+    "
+    >
+      {/*Container*/}
+      <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[5%]">
+        {/*Messages*/}
+        {messages &&
+          messages.map((message) => (
+            <>
+              {/*Message files */}
+              {message.files.length > 0
+                ? message.files.map((file) => (
+                    <FileMessage
+                      FileMessage={file}
+                      message={message}
+                      key={message._id}
+                      me={user._id === message.sender._id}
+                    />
+                  ))
+                : null}
+              {/*Message text*/}
+              {message.message.length > 0 ? (
+                <Message
+                  message={message}
+                  key={message._id}
+                  me={user._id === message.sender._id}
+                />
+              ) : null}
+            </>
+          ))}
+        {typing === activeConversation._id ? <Typing /> : null}
+        <div className="mt-2" ref={endRef}></div>
+      </div>
     </div>
   );
 }
